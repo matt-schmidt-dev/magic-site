8bd40c7e1b40760bc55626ece592f73351c46c9f
Matthew Schmidt <47067179+matts0809@users.noreply.github.com>
2019-05-07 18:17:13 -0500

    updated

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ document.querySelector('.burger-exit').classList.toggle('active-exit-burger');
     $(document).ready(function(){
       $('.burger-exit').click(function(){
         $('.nav-ul-list').removeClass('nav-active');
-        $('.burger-exit').removeClass('active-exit-burger')
+        $('.burger-exit').removeClass('active-exit-burger');
       });
     });
 
@@ -78,8 +78,15 @@ $(window).scroll(function() {
 
 
 //Scroll Animation for Services Section
+
 $(window).scroll(function() {
   if ($(document).scrollTop() > 300) {
+    $('.ser-sect-par').addClass('ser-sect-par-scroll');
+  }
+});
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 550) {
     $('.s1-des, .s1-img').addClass('s123-scroll');
   }
   /*else {
@@ -88,13 +95,13 @@ $(window).scroll(function() {
 });
 
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 700) {
+  if ($(document).scrollTop() > 950) {
     $('.s2-des, .s2-img').addClass('s123-scroll');
   }
 });
 
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 1100) {
+  if ($(document).scrollTop() > 1250) {
     $('.s3-des, .s3-img').addClass('s123-scroll');
   }
 });
@@ -116,19 +123,19 @@ document.querySelector('.s1-popup-container').classList.toggle('s1-popup-contain
 
 //Animation for Gallery Section
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 1600) {
+  if ($(document).scrollTop() > 1700) {
     $('.one, .two, .three').addClass('grid-portfolio-item-scroll');
   }
 });
 
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 1850) {
+  if ($(document).scrollTop() > 1950) {
     $('.four, .five, .six').addClass('grid-portfolio-item-scroll');
   }
 });
 
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 2100) {
+  if ($(document).scrollTop() > 2200) {
     $('.seven, .eight, .nine').addClass('grid-portfolio-item-scroll');
   }
 });
